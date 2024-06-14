92cbc050ce278747a5b41586b6cec430c1635a89
Kaiswim1 <68935355+Kaiswim1@users.noreply.github.com>
2024-06-14 00:57:52 -0400

    Update Cloner.cs
    Fixed bugs related to clones creating clones.

--- Cloner.cs
+++ Cloner.cs
@@ -2,43 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cloner
+public class Cloner : MonoBehaviour
 {
-    // Start is called before the first frame update  
-    private static int cloneIterator = 0;   
+    private static int cloneIterator = 0;    
+    private static GameObject clone;  
+    private static int numberOfAllClones = 0;  
 
-    /**
-    * stitchHorizontal will stitch gameobjects to the left or right of the 
-    */ 
-    private static void stitchHelperH(GameObject g, int manyClones, int direction){
-         while (GameObject.FindGameObjectsWithTag(g.tag).Length <= manyClones){
-            cloneIterator+=direction; 
-            GameObject.Instantiate(g, new Vector3(g.GetComponent<SpriteRenderer>().bounds.size.x * cloneIterator, g.transform.position.y, 0), Quaternion.identity);  
-        }  
+    private static void stitchHelperH(GameObject g, int manyClones, int direction){               
+            Vector3 origin = g.transform.position; 
+            cloneIterator=direction; 
+            numberOfAllClones+=manyClones; 
+            while (GameObject.FindGameObjectsWithTag(g.tag).Length <= numberOfAllClones){
+                    clone = GameObject.Instantiate(g, new Vector3( g.transform.position.x + (g.GetComponent<SpriteRenderer>().bounds.size.x * cloneIterator), g.transform.position.y, 0), Quaternion.identity); 
+                    cloneIterator+=direction;  
+            }     
+            g.transform.position=origin; 
+            
     } 
-    private static void stitchHelperV(GameObject g, int manyClones, int direction){
-         while (GameObject.FindGameObjectsWithTag(g.tag).Length <= manyClones){
-            cloneIterator+=direction; 
-            GameObject.Instantiate(g, new Vector3(g.transform.position.x, g.GetComponent<SpriteRenderer>().bounds.size.y * cloneIterator, 0), Quaternion.identity);  
-        }  
-    }
 
-    public static void stitchRight(GameObject g,int manyClones){stitchHelperH(g,manyClones,1);}       
-    public static void stitchLeft(GameObject g,int manyClones){stitchHelperH(g,manyClones,-1);}    
-    public static void stitchUp(GameObject g,int manyClones){stitchHelperH(g,manyClones,1);}       
-    public static void stitchDown(GameObject g,int manyClones){stitchHelperH(g,manyClones,-1);}   
-
-
+    private static void stitchHelperV(GameObject g, int manyClones, int direction){  
+            Vector3 origin = g.transform.position; 
+            cloneIterator=direction; 
+            numberOfAllClones+=manyClones; 
+            while (GameObject.FindGameObjectsWithTag(g.tag).Length <= numberOfAllClones){
+                    clone = GameObject.Instantiate(g, new Vector3( g.transform.position.x, g.transform.position.y + (g.GetComponent<SpriteRenderer>().bounds.size.y * cloneIterator), 0), Quaternion.identity); 
+                    cloneIterator+=direction;  
+            }     
+            g.transform.position=origin; 
+            
+        
+    } 
 
 
     
-    public static void spawnProjectile(GameObject g, Vector3 location, Vector3 velocity, int lifetimeMillis){
+    public static void stitchRight(GameObject g,int manyClones){ 
+        stitchHelperH(g,manyClones,1);      
+    }       
+    public static void stitchLeft(GameObject g,int manyClones){
+        stitchHelperH(g,manyClones,-1);
+    }    
+    public static void stitchUp(GameObject g,int manyClones){
+        stitchHelperV(g,manyClones,1);
+    }       
+    public static void stitchDown(GameObject g,int manyClones){
+        stitchHelperV(g,manyClones,-1);
+    }   
+    
+
+    public static void spawnProjectile(GameObject g, Vector3 velocity, int lifetimeMillis){
         if (!g.CompareTag("Clone")){
-            GameObject clone = GameObject.Instantiate(g, location, Quaternion.identity);
+            GameObject clone = GameObject.Instantiate(g, g.transform.position, Quaternion.identity);
             clone.tag = "Clone";
             clone.GetComponent<Rigidbody>().AddForce(velocity, ForceMode.Impulse);
             GameObject.Destroy(clone, lifetimeMillis / 1000f);
         }  
     }
-
 }
